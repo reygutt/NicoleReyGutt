f868329e062536b17860f259a98e1b2f52a42b77
Nicole Rey <nicolereyesgg@hotmail.com>
2020-01-04 21:53:25 +0100

    navbar and color change background

--- assets/JS/navbar.js
+++ assets/JS/navbar.js
@@ -8,9 +8,11 @@ function cancelIcon(x) {
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 let navBar = document.getElementsByClassName('navbar')[0];
+let navBarBackground = document.getElementById('navbar-background');
 
 navBarToggle.addEventListener('click', function() {
     mainNav.classList.toggle('active');
+    navBarBackground.classList.toggle('navbarbackground');
     navBar.classList.toggle('activecolor');
     //navBar.style.backgroundColor = "#e1bbff"; //CHANGE BACKGROUNDCOLOR NAVBAR
 
